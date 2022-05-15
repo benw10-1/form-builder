279202d147b4ce52b207942ce49c46aa7ff62b42
Ben Wirth <benjaminwirth10@gmail.com>
2022-05-14 21:34:31 -0700

    comments

--- server/schemas/resolvers/queries.js
+++ server/schemas/resolvers/queries.js
@@ -2,6 +2,7 @@ const { User } = require("../../models")
 const { AuthenticationError } = require('apollo-server-express')
 
 function thisUser(parent, args, context) {
+    // get user and also return full forms object
     if (context.user) return User.findOne({ _id: context.user._id }).populate("forms")
     throw new AuthenticationError("Not logged in")
 }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,13 +5,17 @@ const expiration = '2h'
 
 module.exports = {
     authMiddleware: function ({ req }) {
+        // get token from header, body, or query
         let token = req.body.token || req.query.token || req.headers.authorization
 
+        // get actual token part minus Bearer
         if (req.headers.authorization) token = token.split(' ').pop().trim()
 
+        // guard clause
         if (!token) return req
 
         try {
+            // verify that no changes have been make to the token
             const { data } = jwt.verify(token, process.env.SECRET, { maxAge: expiration })
             req.user = data
         } catch {
