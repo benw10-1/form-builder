0df1c93e20078f4929bccde1d6b1e84e3ebfae7d
Ignaz Protagonist <pilibili@protonmail.com>
2022-06-01 21:11:56 -0500

    barely anything

--- client/src/pages/ALTEditForm.js
+++ client/src/pages/ALTEditForm.js
@@ -1276,7 +1276,7 @@ This is unnecessary (and doesn't work) because shallow references? disconcerting
 
             <div className="rightButtons">
                 <ButtonsTwo conf={confirm} key={"buttons2"}/>
-                <ButtonsOne form={formRef.current} key={"buttons1"}/>
+                <ButtonsOne form={pieces} key={"buttons1"}/>
             </div>
             
         </Box>
