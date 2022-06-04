f989e81e23aecb79edcc3144c9cd1d35c8acde28
Ben Wirth <benjaminwirth10@gmail.com>
2022-06-04 01:36:07 -0700

    responses start

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import {
 
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import { Auth } from "./utils"
+import "./App.css";
 
 function App() {
     return (
@@ -35,13 +36,7 @@ function App() {
                 {/*testing route*/}
                 <Route path="/alteditform/:id" element={<ALTEditForm/>} />
                 <Route path="/respond/:id" element={<Respond/>} />
-                <Route path="/testResponse" element={(
-                    <div style={{ width: "100%", display: "grid", placeItems: "center" }}>
-                        <div style={{ width: "60%" }}>
-                            <Responses/>
-                        </div>
-                    </div>
-                )} />
+                <Route path="/testResponse" element={<Responses/>} />
 
                 {/* wrong route route */}
                 <Route path="*" element={<FourOFour />}/>
